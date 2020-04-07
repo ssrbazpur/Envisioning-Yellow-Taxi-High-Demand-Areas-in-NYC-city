a201116f05e0e3be38c9516f22e5c0fee07d40cd
Simranjeet Singh Randhawa <ssrbazpur@gmail.com>
2020-04-06 19:48:12 -0600

    Add files via upload

--- Operationalize/Flask app/main.py
+++ Operationalize/Flask app/main.py
@@ -1,13 +1,21 @@
 ############################################################
 #                                                          # 
 #   ENVISIONING YELLOW TAXI CAB DEMAND IN NYC CITY         #
+#       SUBMITTED BY : SIMRANJEET RANDHAWA                 #
+#                                                          #
 #                                                          #
 ############################################################
 
 
 
 
-# ___HEADER FILES___
+
+############################################################
+#                                                          # 
+#                IMPORTING HEADER FILES                    #    
+#                                                          #
+############################################################
+
 
 from flask import flash,Flask,render_template,request,url_for
 from flask import jsonify
@@ -31,7 +39,11 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
 
-# __Flask Application__
+############################################################
+#                                                          # 
+#       CALLING FLASK APPLICATION                          #    
+#                                                          #
+############################################################
 
 app = Flask(__name__)
 
@@ -39,7 +51,7 @@ app = Flask(__name__)
 
 model=joblib.load('final2.pkl')
 
-# __Importing my Google Cloud Credentials__
+# __Importing my Google Cloud Credentials____
 
 import os
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="nyc-taxi-265120-baf9a3e4cf9b.json"
@@ -52,13 +64,21 @@ client = bigquery.Client(project = project_id)
 USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
 LANGUAGE = "en-US,en;q=0.5"
 
+
+############################################################
+#                                                          # 
+#       LIVE WEATHER SCRAPPER FROM GOOGLE URL              #    
+#                                                          #
+############################################################
+
+# url : The parameter that consist the url of Google Weather Feed
 def get_weather_data(url):
     session = requests.Session()
     session.headers['User-Agent'] = USER_AGENT
     session.headers['Accept-Language'] = LANGUAGE
     session.headers['Content-Language'] = LANGUAGE
     html = session.get(url)
-    # create a new soup
+    # create a soup
     soup = bs(html.text, "html.parser")
     # store all results on this dictionary
     result = {}
@@ -74,82 +94,116 @@ def get_weather_data(url):
     next_days = []
     days = soup.find("div", attrs={"id": "wob_dp"})
     for day in days.findAll("div", attrs={"class": "wob_df"}):
-        # extract the name of the day
         day_name = day.find("div", attrs={"class": "vk_lgy"}).attrs['aria-label']
-        # get weather status for that day
         weather = day.find("img").attrs["alt"]
         temp = day.findAll("span", {"class": "wob_t"})
-        # maximum temparature in Celsius, use temp[1].text if you want fahrenheit
-        max_temp = temp[0].text
-        # minimum temparature in Celsius, use temp[3].text if you want fahrenheit
-        min_temp = temp[2].text
+        max_temp = min(int(temp[1].text),int(temp[0].text))
+        min_temp = min(int(temp[3].text),int(temp[2].text))
         next_days.append({"name": day_name, "weather": weather, "max_temp": max_temp, "min_temp": min_temp})
-    # append to result
     result['next_days'] = next_days
     return result
 
-##Close New Line
+############################################################
+#                                                          # 
+#                CONVERT DATE TO NTH DAY                   #    
+#                                                          #
+############################################################
+
+def date_to_nth_day(date, format='%Y-%m-%d'):
+    date = pd.to_datetime(date, format=format)
+    new_year_day = pd.Timestamp(year=date.year, month=1, day=1)
+    return (date - new_year_day).days + 1
+
+############################################################
+#                                                          # 
+#             CREATING ROUTES IN FLASK APP                 #    
+#                                                          #
+############################################################
+    
 
 
-# ___Main Route___
+# _____Route / renders index.html in template folder______
 
 @app.route("/")
 def index():
 	return render_template("index.html")
 
-# ___predict Route__
+# _____predict Route renders result.html if no error______
     
 @app.route('/predict',methods=['POST'])
 def predict():
-	
     region="New York"
     URL="https://www.google.com/search?lr=lang_en&ie=UTF-8&q=weather"
     URL += region
-    data = get_weather_data(URL)
-    # print data
-    print("Weather for:", data["region"])
-    print("Now:", data["dayhour"])
-    
-    print("Description:", data['weather_now'])
-    print("Precipitation:", data["precipitation"])
-    print("Humidity:", data["humidity"])
-    print("Wind:", data["wind"])
-    print("Next days:")
+    ###########  Getting live weather feed ####################
+    data = get_weather_data(URL)    
     for dayweather in data["next_days"]:
         print("="*40, dayweather["name"], "="*40)
-        print("Description:", dayweather["weather"])
-    print(data["next_days"][0])		
+        print("Description:", dayweather["weather"])	
+    ###########  Getting index.html Form data ####################
     int_features = [x for x in request.form.values()]
     date=int_features[0]
-    lat=int_features[1]
     output = int_features[1]
     input1=int_features[2]
+    df=pd.read_csv("temp.csv")
+    tempdate = datetime.datetime.strptime(date, "%Y-%m-%dT%H:%M")
+    nthday=date_to_nth_day(tempdate)
+    print("The nthday is as",nthday)
+    enterdatemintemp=df.loc[df['Day '] == nthday]['Min '].iloc[0]
+    enterdatemaxtemp=df.loc[df['Day '] == nthday]['Max'].iloc[0]
     query="""SELECT * FROM `bigquery-public-data.new_york_taxi_trips.taxi_zone_geom` WHERE ST_CONTAINS(zone_geom, ST_GEOGPOINT({1}, {0}))""".format(output,input1)
     df = client.query(query).to_dataframe()
+    ###########  TRY CATCH BLOCK ####################
     try:
+        ######### Getting Information about the Zone ##########
         a=df.iloc[0]['zone_name']
+        b=df.iloc[0]['borough']
         print(df.head())
         first=df.iloc[0]['zone_id']
         print(date) 
         my_date = datetime.datetime.strptime(date, "%Y-%m-%dT%H:%M")
         time=data["dayhour"]
         temp=data['temp_now']
+        noting = 0
+        for dayweather in data["next_days"]:
+            print("="*40, dayweather["name"], "="*40)
+            print("Description:", dayweather["weather"])
+            if dayweather["name"][0]=='r':
+                noting=1
+        ########## Getting the data into appropriate format #########
         fourth=my_date.weekday()
         third=my_date.hour
+        period=third+1
         second=my_date.timetuple().tm_yday
-        print(second)
+        daynumbervalue=int(second)
+        print("The daynumber is as",daynumbervalue)
+        todaydate=datetime.datetime.today()
+        todayday=int(todaydate.timetuple().tm_yday)
+        print("Today date is as",todayday)
+        for i in range(0,7):
+            if i+todayday== daynumbervalue:
+                print("Value of I IS",i)
+                enterdatemintemp=data["next_days"][i]['min_temp']
+                enterdatemaxtemp=data["next_days"][i]['max_temp']
         six=data["next_days"][0]['max_temp']
         five=data["next_days"][0]['min_temp']
-        ans=model.predict([[first,second,third,fourth,five,six,0]])
+        ###########  Predicting the result using the model ####################
+        ans=model.predict([[first,second,third,fourth,five,six,noting]])
     
         ans=int(ans)
-        return render_template('results.html',temp=temp,time=time,ZoneName=a,zoneid=first, prediction_text='Zone is {0} and Number of trips will be  {1}'.format(a,ans),mtemp1=data["next_days"][0]['min_temp'],temp1=data["next_days"][0]['max_temp'],mtemp2=data["next_days"][1]['min_temp'],temp2=data["next_days"][1]['max_temp'],temp3=data["next_days"][2]['max_temp'],temp4=data["next_days"][3]['max_temp'],temp5=data["next_days"][4]['max_temp'],temp6=data["next_days"][5]['max_temp'],temp7=data["next_days"][6]['max_temp'],mtemp3=data["next_days"][2]['min_temp'],mtemp4=data["next_days"][3]['min_temp'],mtemp5=data["next_days"][4]['min_temp'],mtemp6=data["next_days"][5]['min_temp'],mtemp7=data["next_days"][6]['min_temp'])
+        return render_template('results.html',entermin=enterdatemintemp,entermax=enterdatemaxtemp,da1=my_date,lat=output,long=input1,time1=third,temp=temp,time=time,boro=b,ZoneName=a,zoneid=first,trips=ans, prediction_text='Zone is {0} and Number of trips will be  {1}'.format(a,ans),mtemp1=data["next_days"][0]['min_temp'],temp1=data["next_days"][0]['max_temp'],mtemp2=data["next_days"][1]['min_temp'],temp2=data["next_days"][1]['max_temp'],temp3=data["next_days"][2]['max_temp'],temp4=data["next_days"][3]['max_temp'],temp5=data["next_days"][4]['max_temp'],temp6=data["next_days"][5]['max_temp'],temp7=data["next_days"][6]['max_temp'],mtemp3=data["next_days"][2]['min_temp'],mtemp4=data["next_days"][3]['min_temp'],mtemp5=data["next_days"][4]['min_temp'],mtemp6=data["next_days"][5]['min_temp'],mtemp7=data["next_days"][6]['min_temp'])
     except:
         return render_template('error.html')
 
 
+# _____weekpred Route to render index1.html______
 
-# ___go12 Route (for Weekly Prediction)____
+@app.route('/weekpred')
+def weekpred():
+    
+    return render_template('index1.html')
+
+# ___go12 Route (for Weekly Prediction) and rendering index.html____
 
 @app.route('/go12')
 def go12():
@@ -183,6 +237,8 @@ def go12():
     l3=second*263*24
     l4=third*263*7
     l5=four*263*24
+    
+    ####### Creation of Dataframe ############
     df = pd.DataFrame(list(zip(l2, a1,l3,l4,l5)), 
                columns =['Zone', 'ActualDate','DayNumber','Hour','DayofwEEK']) 
     region="New York"
@@ -215,6 +271,7 @@ def go12():
         max1.append(dayweather['max_temp'])
     result1=[]
     result2=[]
+    ########### Making Dataframe for the next week ###########
     for value in df["DayofwEEK"]: 
       
         if value == (week %7): 
@@ -247,21 +304,36 @@ def go12():
     rain=[]
     for i in range(0,44184):
         rain.append(0)
+    
+    for dayweather in data["next_days"]:
+        if "rain" in dayweather["weather"]:
+            rain=1
     df["rain"]=rain
     print(df.head())
     ans=model.predict(df[['Zone', 'DayNumber','Hour','DayofwEEK','min','max','rain']] )
-    print("hello")
     df['ans']=ans
     df.loc[df['ans'] < 0, 'ans'] = 0
+    df["ans"] = df["ans"].astype(int)
     gbq.to_gbq(df, 'hello.test_table1', 'nyc-taxi-265120', if_exists='replace')
     return render_template('index1.html')
 
 
-
-# ___Main Function____
+############################################################
+#                                                          # 
+#                   MAIN FUNCTION                          #    
+#                                                          #
+############################################################
     
 if __name__ == '__main__':
 	app.run(host="127.0.0.1",port=8080,debug=True)
+    
+    
+    
+############################################################
+#                                                          # 
+#                THANK YOU FOR VIEWING                     #    
+#                                                          #
+############################################################    
     
     
 
